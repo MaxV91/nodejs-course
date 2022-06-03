5dff45fc41e3098faa23b8cd26d5b6c0c0b127fc
Maxim K <max.kotsurenko@gmail.com>
2022-06-03 10:45:00 +0300

    routing/redirecting requests

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //console.log(req.url, req.method, req.headers);
   //process.exit();
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html lang="en">');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/')
+    return res.end();
+  }
   res.setHeader('Content-type', 'text/html');
   res.write('<html lang="en">');
   res.write('<head><title>My first page</title></head>');
