63d53c02e5f7e9db29b3b87e9825248259015266
Maxim K <max.kotsurenko@gmail.com>
2022-06-24 22:03:54 +0300

    router, 404

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req,res, next) => {
-  console.log('+');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req,res, next) => {
-  res.send('<h1>hello express</h1>');
-});
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
