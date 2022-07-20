6930216c56fe450528a40a3e6366bb8eeb1f36cf
Maxim K <max.kotsurenko@gmail.com>
2022-07-21 00:36:10 +0300

    deleting items

--- models/cart.js
+++ models/cart.js
@@ -34,6 +34,21 @@ module.exports = class Cart {
         console.log(err);
       })
     })
+  }
 
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = {...JSON.parse(fileContent)};
+      const product = updatedCart.products.find(product => product.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(product => product.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      })
+    })
   }
 }
