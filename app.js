d311d503ba836bedc53bece73ee245072c18c799
Maxim K <max.kotsurenko@gmail.com>
2022-08-09 00:06:06 +0300

    added cart model, add/delete items

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,13 +22,51 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+
+sequelize
+  //.sync({ force: true })
+  .sync()
   .then(result => {
+    return User.findById(1);
+    //app.listen(3000);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        name: 'Max',
+        email: 'max@mail.to'
+      })
+    }
+    return user;
+  })
+  .then(user => {
+    //console.log(user);
+    return user.createCart();
+  })
+  .then(user => {
     app.listen(3000);
   })
   .catch(err => {
