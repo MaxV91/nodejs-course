f35c5bf60f91b3ea272ca16b86db35e966fd922c
Maxim K <max.kotsurenko@gmail.com>
2022-08-11 23:34:27 +0300

    connect to mongodb

--- util/database.js
+++ util/database.js
@@ -1,7 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
-  dialect: 'mysql', host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb://node-course.loc:27017')
+    .then(client => {
+      console.log('connected');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+module.mongoConnect = mongoConnect;
+module.getDb = getDb;
